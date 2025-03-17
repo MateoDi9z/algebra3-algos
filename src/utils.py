2fb43574ce8050ee78666999ea22482f321200ab
MateoDi9z <dzmateozd@gmail.com>
2025-03-17 00:34:09 -0300

    utils & ex1 a,b,c

---
+++ src/utils.py
@@ -0,0 +1,30 @@
+import time
+import sys
+
+
+n = 1000000
+
+
+def setup(t=n):
+    global n
+    n = t
+    sys.setrecursionlimit(t + 10)
+
+
+def Line(): print("========================")
+
+
+def Timer(ex: str, f, recursive: bool, param=False):
+    ini = time.time()
+
+    if param:
+        result = f(n, param)
+    else:
+        result = f(n)
+
+    end = time.time()
+
+    if recursive:
+        print(f"{ex}) recurs. time: \t{round(end - ini, 4)}s")
+    else:
+        print(f"{ex}) iter. time: \t\t{round(end - ini, 4)}s")
